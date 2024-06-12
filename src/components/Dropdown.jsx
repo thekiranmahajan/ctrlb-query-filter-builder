1a7d7b055ebc4c998621d1401d19e91894bcf823
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-12 19:19:30 +0530

    feat: dynamically render Dropdown based on step also filter the user input text from options

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { attributes, operations } from "../utils/constants";
 
-const Dropdown = ({ options, onSelect }) => {
+const Dropdown = ({ step, inputValue, onSelect }) => {
+  const getDropdownOptions = () => {
+    if (step === "attribute") return attributes;
+    if (step === "operation") return operations;
+    return [];
+  };
   return (
     <div className="bg-[#121317] z-10 w-full mt-2 rounded p-1 flex flex-col justify-start gap-1 ">
-      {options.map((option, index) => (
-        <div
-          key={index}
-          onClick={() => onSelect(option)}
-          className="h-10 w-full hover:bg-slate-800 rounded flex px-3 items-center cursor-pointer"
-        >
-          {option}
-        </div>
-      ))}
+      {getDropdownOptions()
+        .filter((option) =>
+          option.toLowerCase().includes(inputValue.toLowerCase())
+        )
+        .map((option, index) => (
+          <div
+            key={index}
+            onClick={() => onSelect(option)}
+            className="h-10 w-full hover:bg-slate-800 rounded flex px-3 items-center cursor-pointer"
+          >
+            {option}
+          </div>
+        ))}
     </div>
   );
 };

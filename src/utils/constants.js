ae8ea352caad99d3b016fa6f732cf5e0415dd8d3
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-12 18:04:05 +0530

    feat: create constants i.e. hardcoded attributes and operations

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+export const attributes = [
+  "conatiner_id",
+  "container_name",
+  "severity_number",
+  "body",
+  "trace_id",
+  "span_id",
+  "trace_flags",
+  "severity_text",
+];
+
+export const operations = [
+  "=",
+  "!=",
+  "IN",
+  "NOT_IN",
+  "LIKE",
+  "NOT_LIKE",
+  "CONTAINS",
+];

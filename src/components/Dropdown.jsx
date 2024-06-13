8cc15953166fbd4c93e10c816a2a726e506b74d2
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-13 13:50:00 +0530

    feat: add arrowUp and down support to select options from dropdown with indications + all minor fixes

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,22 +1,40 @@
-import React from "react";
-import { getDropdownOptions } from "../utils/constants";
+import { useEffect, useRef } from "react";
 
-const Dropdown = ({ step, inputValue, onSelect }) => {
+const Dropdown = ({
+  focusedIndex,
+  options,
+  step,
+  inputValue,
+  onSelect,
+  currentQuery,
+}) => {
+  const optionContainer = useRef(null);
+  useEffect(() => {
+    if (!optionContainer.current) return;
+    optionContainer.current.scrollIntoView({
+      block: "center",
+    });
+  }, [focusedIndex]);
+
   return (
-    <div className="bg-[#121317] z-10 w-full mt-2 rounded p-1 flex flex-col justify-start gap-1 ">
-      {getDropdownOptions(step)
+    <div className="bg-[#121317] z-10 w-full mt-2 rounded p-1 flex flex-col justify-start gap-1  overflow-y-auto overflow-x-hidden">
+      {options
         .filter((option) =>
           option.toLowerCase().includes(inputValue.toLowerCase())
         )
         .map((option, index) => (
           <div
             key={index}
+            ref={index === focusedIndex ? optionContainer : null}
             onClick={() => onSelect(option)}
-            className="h-10 w-full hover:bg-slate-800 rounded flex px-3 items-center cursor-pointer"
+            className={` h-10 w-full active:bg-[#434650] rounded flex px-3 items-center cursor-pointer ${
+              index === focusedIndex ? "bg-[#434650]" : ""
+            }`}
             role="button"
             tabIndex={0}
             aria-label={option}
           >
+            {step === "operation" && `${currentQuery?.attribute} `}
             {option}
           </div>
         ))}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,12 +3,14 @@ import {
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Dropdown from "./Dropdown";
 import TripletFilter from "./TripletFilter";
+import { getDropdownOptions } from "../utils/constants";
 
 const SearchBar = () => {
   const [queries, setQueries] = useState([]);
+  const [options, setOptions] = useState([]);
   const [currentQuery, setCurrentQuery] = useState({
     attribute: "",
     operation: "",
@@ -17,10 +19,11 @@ const SearchBar = () => {
   const [step, setStep] = useState("attribute");
   const [inputValue, setInputValue] = useState("");
   const [isDropdown, setIsDropdown] = useState(false);
+  const [focusedIndex, setFocusedIndex] = useState(-1);
 
-  const handleInputClick = () => {
-    setIsDropdown(true);
-  };
+  useEffect(() => {
+    setOptions(getDropdownOptions(step));
+  }, [step]);
 
   const handleSelect = (selectedValue) => {
     if (step === "attribute") {
@@ -30,14 +33,17 @@ const SearchBar = () => {
         value: "",
       });
       setStep("operation");
+      setIsDropdown(true);
     } else if (step === "operation") {
       setCurrentQuery({
         ...currentQuery,
         operation: selectedValue,
       });
       setStep("value");
+      setIsDropdown(false);
     }
     setInputValue("");
+    setFocusedIndex(-1);
   };
 
   const handleValueChange = (e) => {
@@ -50,8 +56,7 @@ const SearchBar = () => {
 
   const handleAddQuery = (e) => {
     e.preventDefault();
-    if (e.key !== "Enter") return;
-    if (step === "value" && currentQuery.value) {
+    if (e.key === "Enter" && step === "value" && currentQuery.value) {
       setQueries([...queries, currentQuery]);
       setCurrentQuery({
         attribute: "",
@@ -60,7 +65,8 @@ const SearchBar = () => {
       });
       setStep("attribute");
       setInputValue("");
-      console.log(currentQuery.value);
+      setFocusedIndex(-1);
+      setIsDropdown(true);
     }
   };
 
@@ -68,27 +74,80 @@ const SearchBar = () => {
     setQueries(queries.filter((_, i) => i !== index));
   };
 
+  const handleKeyDown = (e) => {
+    const { key } = e;
+    let nextIndexCount = focusedIndex;
+    if (key === "ArrowDown") {
+      nextIndexCount = (focusedIndex + 1) % options.length;
+      setFocusedIndex(nextIndexCount);
+      return;
+    }
+    if (key === "ArrowUp") {
+      nextIndexCount = (focusedIndex + options.length - 1) % options.length;
+      setFocusedIndex(nextIndexCount);
+      return;
+    }
+    if (key === "Enter" && step !== "value") {
+      if (options[focusedIndex]) {
+        handleSelect(options[focusedIndex]);
+      }
+      return;
+    }
+  };
+
+  const getDisplayValue = () => {
+    if (step === "attribute") return inputValue;
+    if (step === "operation") return `${currentQuery.attribute} ${inputValue}`;
+    if (step === "value") return currentQuery.value;
+    return inputValue;
+  };
+
+  useEffect(() => {
+    if (step === "operation" && currentQuery.attribute) {
+      setIsDropdown(true);
+    }
+  }, [step, currentQuery]);
+
+  useEffect(() => {
+    if (options.length === 0) {
+      setIsDropdown(false);
+    }
+  }, [options]);
   return (
     <>
-      <div className="h-11  w-full flex items-center justify-center relative bg-[#17181d] border-2 border-[#1f212c] rounded gap-1">
+      <div
+        tabIndex={1}
+        className="h-11  w-full flex items-center justify-center relative bg-[#17181d] border-2 border-[#1f212c] rounded gap-1"
+        onKeyDown={handleKeyDown}
+      >
         <TripletFilter queries={queries} removeQuery={removeQuery} />
         <input
           className="h-full flex-grow bg-transparent px-3 outline-none shadow-lg placeholder:text-sm placeholder:text-[#3f4044] pr-10"
           placeholder='Search Filter: select options from suggested values, for IN/NOT IN operators - press "Enter" after selecting options'
           type="text"
-          value={step === "value" ? currentQuery.value : inputValue}
-          onClick={handleInputClick}
+          value={getDisplayValue()}
+          onClick={() => setIsDropdown(true)}
           onChange={handleValueChange}
           onKeyUp={handleAddQuery}
+          aria-label="Search filter"
         />
         <FontAwesomeIcon
-          onClick={() => !isDropdown && step !== "value" && setIsDropdown(true)}
+          onClick={() => setIsDropdown(!isDropdown && step !== "value")}
           className="absolute z-10 right-4 text-sm text-[#3f4044]"
           icon={isDropdown ? faMagnifyingGlass : faChevronDown}
+          aria-hidden="true"
         />
       </div>
+
       {isDropdown && step !== "value" && (
-        <Dropdown step={step} inputValue={inputValue} onSelect={handleSelect} />
+        <Dropdown
+          focusedIndex={focusedIndex}
+          options={options}
+          step={step}
+          inputValue={inputValue}
+          onSelect={handleSelect}
+          currentQuery={currentQuery}
+        />
       )}
     </>
   );
